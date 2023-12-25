aab5e4f55c400a858b4453d76efdcaaa7a888404
amonge20 <2023_aitor.monge@iticbcn.cat>
2023-12-25 19:28:09 +0100

    Parte 1: Crear 4 botones i que se muestre la tabla del JSON

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //VARIABLES JSON
 let pokemons, municipis, meteorits, movies;
+
 //PART 0
 // POKEMONS
 fetch("js/data/pokemon.json")
@@ -8,6 +9,7 @@ fetch("js/data/pokemon.json")
 	pokemons = data.pokemon;
 	let pokemonArray = pokemons.map((pokemon) => pokemon.name);
 	console.log(pokemonArray);
+	printList();
 });
 
 // MUNICIPIS
@@ -36,3 +38,49 @@ fetch("js/data/movies.json")
 	let moviesArray = movies.map((movie) => movie.title);
 	console.log(moviesArray);
 });
+
+//PART 1
+//ES REINICIA LA PAGINA WEB CADA COP QUE ES EXECUTI LA FUNCIO
+function reinicia(){
+	location.reload();
+}
+//Ordre Ascendent o Descendent
+function orderList(orden){
+	let listado;
+	switch (orden){
+		case "asc":
+			listado = pokemons;
+			listado.sort((a,b) => a.name.localeCompare(b.name));
+			break;
+		case "desc":
+			listado = pokemons;
+			listado.sort((a, b) => b.name.localeCompare(a.name));
+			break;
+		default:
+			alert("NOPE");
+	}
+	printList();
+}
+//Busca la llista del JSON del pokemon, pel·licula, meteorits i municipis
+function  searchList(){
+	let busqueda = prompt("Busca algo?");
+	let resultado = pokemons.findIndex(pokemon => pokemon.name === busqueda);
+	alert(`Elemento en tal posición: ${resultado !== -1 ? resultado : "NOT FOUND"}`);
+}
+//Array multidimensional
+//Per calcular la mitjana amb un array JSON
+function calcMitjana(){
+	let caracteristicasPokemon = pokemons[pokemons.length - 1].weight;
+	let pesoNumericoPokemon = parseFloat(caracteristicasPokemon);
+	alert(`Media del ultimo bloque añadido: ${isNaN(pesoNumericoPokemon) ? 0 : pesoNumericoPokemon.toFixed(2)}`);
+}
+//Funcio per a que es mostri la taula del JSON amb el DOM
+function printList() {
+	let table = '<table id="pokemonTable" style="border-collapse: collapse; width: 100%;"><tr><th>ID</th><th>Imagen</th><th>Nombre</th><th>Peso</th></tr>';
+		pokemons.forEach(pokemon => {
+			let pesoNumericoPokemon = parseFloat(pokemon.weight);
+			table += `<tr><td>${pokemon.id}</td><td><img src="${pokemon.img}" alt="${pokemon.name}" style="max-width: 50px; max-height: 50px;"></td><td>${pokemon.name}</td><td>${isNaN(pesoNumericoPokemon) ? 0 : pesoNumericoPokemon.toFixed(2)}</td></tr>`;
+		});
+		table += '</table>';
+		document.getElementById("resultat").innerHTML = table;
+  }
